d28494808421f6b901fc45748cc1518a2a0b3819
vinodt31 <vinodt31@gmail.com>
2024-03-05 13:27:57 +0530

    Update hotel.controller.js

--- hotel.controller.js
+++ hotel.controller.js
@@ -102,6 +102,7 @@ exports.delete = async function(req, res) {
 };
 
 exports.update = async function(req, res){
+        console.log("response : ")
   return res.send({ status : "success", message: "update details successfully"});
 }
 
